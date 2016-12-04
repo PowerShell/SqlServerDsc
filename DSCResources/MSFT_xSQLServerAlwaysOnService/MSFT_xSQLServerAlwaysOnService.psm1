c379543235c78cdaece211d4c6aa412699135710
Daniel Reist <djreist@outlook.com>
2016-12-04 18:22:09 -0500

    Fixed a typo

--- DSCResources/MSFT_xSQLServerAlwaysOnService/MSFT_xSQLServerAlwaysOnService.psm1
+++ DSCResources/MSFT_xSQLServerAlwaysOnService/MSFT_xSQLServerAlwaysOnService.psm1
@@ -164,7 +164,7 @@ function Test-TargetResource
         $RestartTimeout = 120
     )
     
-    # Determine the current state of Alway On
+    # Determine the current state of Always On
     $params = @{
         Ensure = $Ensure
         SQLServer = $SQLServer
